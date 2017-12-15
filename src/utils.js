0793a7985d07029c35812cd439c3f02ed87bcd5e
Jaron Hamlik <jdhamlik@gmail.com>
2017-12-15 09:51:10 -0800

    Refactor

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,3 @@
 
-exports.workDaysLeftInWeek = (date) => {
-  return Math.max(5 - date.day(), 0)
-}
-
-exports.workDaysPassedInWeek = (date) => {
-  return Math.min(date.day(), 5)
-}
+exports.workDaysLeftInWeek = date => Math.max(5 - date.day(), 0)
+exports.workDaysPassedInWeek = date => Math.min(date.day(), 5)
